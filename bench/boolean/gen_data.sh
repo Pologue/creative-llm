59aff95360fc30c232ba2f30eb4dcf454554ea52
Pologue <412295017@qq.com>
2025-11-03 23:39:19 +0800

    update .gitignore

---
+++ bench/boolean/gen_data.sh
@@ -0,0 +1,1 @@
+python boolean_dataset.py --operators basic --max-depth 2 --output "datasets/boolean_2var.json" --seed 42
---
+++ bench/boolean/run_bench.sh
@@ -0,0 +1,1 @@
+python boolean_benchmark.py --dataset "datasets/boolean_2var.json" --config "config/config_tinyllama.yaml" --n-samples 30 --query-multiplier 1.0 --seed 42
